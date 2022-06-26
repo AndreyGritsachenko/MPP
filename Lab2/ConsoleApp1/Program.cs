53afe0d41a3f6291eda31b1c9afd84a401e703b1
Andrii_Hrytsachenko <ext-andrii-hrytsachenko@rencore.com>
2022-06-26 22:44:13 +0300

    Lab 2

---
+++ Lab2/ConsoleApp1/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            int numberOfThreds = 2;
+            var sum = 0l;
+            TestClass testClass = new TestClass(numberOfThreds);
+            stopwatch.Start();
+
+            testClass.Compute();
+            stopwatch.Stop();
+            
+            sum = sum + stopwatch.ElapsedMilliseconds;
+
+            stopwatch = Stopwatch.StartNew();
+
+            testClass.Compute();
+
+            stopwatch.Stop();
+            sum = sum + stopwatch.ElapsedMilliseconds;
+
+
+            stopwatch = Stopwatch.StartNew();
+
+            testClass.Compute();
+
+            stopwatch.Stop();
+            sum = sum + stopwatch.ElapsedMilliseconds;
+
+
+            stopwatch = Stopwatch.StartNew();
+
+            testClass.Compute();
+
+            stopwatch.Stop();
+            sum = sum + stopwatch.ElapsedMilliseconds;
+
+
+            stopwatch = Stopwatch.StartNew();
+
+            testClass.Compute();
+
+            stopwatch.Stop();
+            sum = sum + stopwatch.ElapsedMilliseconds;
+
+
+            Console.WriteLine($"Total execution time for {numberOfThreds} threads: {sum/5}ms");
+
+            sum = 0;
+            numberOfThreds = 10;
+            testClass = new TestClass(numberOfThreds);
+
+            stopwatch = Stopwatch.StartNew();
+
+            testClass.Compute();
+            stopwatch.Stop();
+
+            sum = sum + stopwatch.ElapsedMilliseconds;
+
+            stopwatch = Stopwatch.StartNew();
+
+            testClass.Compute();
+
+            stopwatch.Stop();
+            sum = sum + stopwatch.ElapsedMilliseconds;
+
+
+            stopwatch = Stopwatch.StartNew();
+
+            testClass.Compute();
+
+            stopwatch.Stop();
+            sum = sum + stopwatch.ElapsedMilliseconds;
+
+
+            stopwatch = Stopwatch.StartNew();
+
+            testClass.Compute();
+
+            stopwatch.Stop();
+            sum = sum + stopwatch.ElapsedMilliseconds;
+
+
+            stopwatch = Stopwatch.StartNew();
+
+            testClass.Compute();
+
+            stopwatch.Stop();
+            sum = sum + stopwatch.ElapsedMilliseconds;
+
+
+            Console.WriteLine($"Total execution time for {numberOfThreds} threads: {sum / 5}ms");
+        }
+
+    }
+}
---
+++ Lab2/ConsoleApp1/TestClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class TestClass
+    {
+        private readonly List<int> list = new List<int>();
+        private readonly int numberOfThreds;
+
+        public TestClass(int numberOfThreds)
+        {
+            list = Enumerable.Range(0,1_000_000).ToList();
+            this.numberOfThreds = numberOfThreds;
+        }
+
+        public static string Transform(int element) => (element*element).ToString().GetHashCode().ToString();
+        
+        public string Compute() 
+        { 
+            ConcurrentBag<string> ts = new ConcurrentBag<string>();
+            Parallel.ForEach(list, new ParallelOptions { MaxDegreeOfParallelism = numberOfThreds }, t => ts.Add(Transform(t)));
+            
+            var arr = ts.ToArray();
+            Array.Sort(arr);
+            return arr[0];
+        }
+    }
+}
